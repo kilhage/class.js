55d28a547e3ec2cb3b509f07ec3e49d32d0bc648
Emil Kilhage <emilkilhage@gmail.com>
2011-05-04 20:38:52 +0200

    added a nice development helper

---
+++ build/dev.js
@@ -0,0 +1,25 @@
+/*jslint node: true, strict: false */
+var u = require("./utils.js"),
+    b = require("./build.js"),
+    fs = require("fs"), 
+    size,
+    type = process.argv[2] || "js";
+
+if (!b.types.hasOwnProperty(type)) {
+    throw "invalid type: " + type;
+}
+
+u.setOutputEnabled(false);
+
+var file = type + ".class.js";
+
+console.log("watching: " + file + "...\n");
+
+fs.watchFile(u.path("src/class.js"), function (curr) {
+    if (!curr.size || size === curr.size) {
+        return;
+    }
+    b.build(type);
+    console.log(file + " updated..");
+    size = curr.size;
+});
